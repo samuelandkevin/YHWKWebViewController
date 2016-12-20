59b6df4d3da1663918e90fa7e3498b5337de71ba
samuelandkevin <YHIOS002@samuelandkevin.local>
2016-12-20 17:23:02 +0800

    Initial Commit

---
+++ YHWKWebViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YHWKWebViewController
+//
+//  Created by YHIOS002 on 16/12/20.
+//  Copyright © 2016年 YHSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YHWKWebViewController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YHWKWebViewController
+//
+//  Created by YHIOS002 on 16/12/20.
+//  Copyright © 2016年 YHSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
